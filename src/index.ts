ac0e0da210fc1a0731cd41a8a34e8db9613e491d
Ukana.Bastreet <s.ppp@bk.ru>
2023-03-30 13:14:36 +0300

    fix export bag

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,15 @@
-import { TreeSelect } from "./TreeSelect/TreeSelect";
-import { Text } from "./Text/Text";
-import { Tag } from "./Tag/Tag";
-import { Tabs } from "./Tabs/Tabs";
-import { Switch } from "./Switch/Switch";
-import { Slot } from "./Slot/Slot";
-import { Pagination } from "./Pagination/Pagination";
-import { Input } from "./Input/Input";
-import { Dropdown } from "./Dropdown/Dropdown";
-import { Checkbox } from "./Checkbox/Checkbox";
-import { Button } from "./Button/Button";
-import { Badge } from "./Badge/Badge";
-import { Anchor } from "./Anchor/Anchor";
-import { Alert } from "./Alert/Alert";
-import { Autocomplete } from "./Autocomplete/Autocomplete";
-
-export {
-  Alert,
-  Anchor,
-  Autocomplete,
-  Badge,
-  Button,
-  Checkbox,
-  Dropdown,
-  Input,
-  Pagination,
-  Slot,
-  Switch,
-  Tabs,
-  Tag,
-  Text,
-  TreeSelect,
-};
+export { TreeSelect } from "./TreeSelect/TreeSelect";
+export { Text } from "./Text/Text";
+export { Tag } from "./Tag/Tag";
+export { Tabs } from "./Tabs/Tabs";
+export { Switch } from "./Switch/Switch";
+export { Slot } from "./Slot/Slot";
+export { Pagination } from "./Pagination/Pagination";
+export { Input } from "./Input/Input";
+export { Dropdown } from "./Dropdown/Dropdown";
+export { Checkbox } from "./Checkbox/Checkbox";
+export { Button } from "./Button/Button";
+export { Badge } from "./Badge/Badge";
+export { Anchor } from "./Anchor/Anchor";
+export { Alert } from "./Alert/Alert";
+export { Autocomplete } from "./Autocomplete/Autocomplete";
