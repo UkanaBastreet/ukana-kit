a70f7cd78e2b0208fec2e6d25175ddc04fb616e8
Ukana.Bastreet <s.ppp@bk.ru>
2023-04-10 01:27:31 +0300

    some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     libraryTarget: "umd",
     clean: true,
+    globalObject: "this",
   },
   resolve: {
     extensions: [".ts", ".tsx"],
