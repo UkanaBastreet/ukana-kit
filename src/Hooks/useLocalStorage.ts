c91e1904d9177fc76b5c0275c1666559b4e7c12d
Ukana.Bastreet <s.ppp@bk.ru>
2023-04-15 12:58:34 +0300

    some fixes

--- src/Hooks/useLocalStorage.ts
+++ src/Hooks/useLocalStorage.ts
@@ -1,16 +1,19 @@
 import { useState } from "react";
 
 export const useLocalStorage = (key: string, initialValue: any = null) => {
-  if (typeof window === "undefined") return [false];
-  const [value, setValue] = useState(() => {
-    const storedValue = localStorage.getItem(key);
-    return storedValue !== null ? JSON.parse(storedValue) : initialValue;
-  });
+  if (typeof window !== "undefined") {
+    const [value, setValue] = useState(() => {
+      const storedValue = localStorage.getItem(key);
+      return storedValue !== null ? JSON.parse(storedValue) : initialValue;
+    });
 
-  const setValueWithLocalStorage = (newValue: any) => {
-    setValue(newValue);
-    localStorage.setItem(key, JSON.stringify(newValue));
-  };
+    const setValueWithLocalStorage = (newValue: any) => {
+      setValue(newValue);
+      localStorage.setItem(key, JSON.stringify(newValue));
+    };
 
-  return [value, setValueWithLocalStorage];
+    return [value, setValueWithLocalStorage];
+  } else {
+    return [false];
+  }
 };
